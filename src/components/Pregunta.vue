948b23b4f15dfc55b6542652ea73c979ff4874ba
joselynM99 <joselynmoncayo1999@gmail.com>
2023-01-07 10:50:05 -0500

    taller17

--- src/components/Pregunta.vue
+++ src/components/Pregunta.vue
@@ -1,13 +1,13 @@
 <template>
   <h1>Pregunta</h1>
-  <img src="https://via.placeholder.com/250" alt="No se puede visualizar" />
+  <img v-bind:src="img" v-if=img alt="No se puede visualizar" />
   <div class="fondo-oscuro"></div>
   <div class="container">
-    <input type="text" placeholder="Hazme una pregunta" />
-    <p>Recuerda terminar con un signo de interrogación</p>
+    <input v-model="question" type="text" placeholder="Hazme una pregunta" />
+    <p>Recuerda terminar con un signo de interrogación (?)</p>
     <div>
-      <h2>Voy a pasar de año?</h2>
-      <h1>SI, NO, ......</h1>
+      <h2>{{ question }}</h2>
+      <h1>{{respuesta}}</h1>
     </div>
   </div>
 </template>
@@ -15,13 +15,58 @@
 <script>
 export default {
   name: "Pregunta",
+
+  data() {
+    return {
+      question:null,
+      respuesta:null,
+      img:null,
+    };
+  },
+  /*wath: observadores de las propiedades reactivas, deben tener el mismo nombre, obtiene el valor actual y antiguo*/
+  watch: {
+    question(value, oldValue) {
+      console.log(oldValue);
+      console.log(value);
+      if (!value.includes("?")) return;
+      console.log("Vamos a consumir el API");
+      //Consumo del API (Conjunto de especificaciones, politicas, que permiten comunicar dos o más componentes de software)
+      this.consumirAPI()
+    },
+  },
+
+  methods: {
+    //Método asíncrono
+    async consumirAPI() {
+      this.respuesta='Pensando.'
+      const {answer, forced, image}= await fetch("https://yesno.wtf/api").then((result) =>
+        result.json()
+      );
+
+      this.respuesta='Pensando...'
+      console.log(forced);
+      console.log(image)
+      console.log(answer)
+      this.respuesta=answer
+      this.img=image
+      this.respuesta='Pensando..'
+  
+    },
+  },
 };
 </script>
 
 <style>
-img {
+img,
+.fondo-oscuro {
   height: 100vh;
   width: 100vw;
+  position: fixed;
+  left: 0px;
+  top: 0px;
+}
+.fondo-oscuro {
+  background-color: rgba(0, 0, 0, 0.5);
 }
 input {
   width: 250px;
@@ -30,16 +75,20 @@ input {
   border: none;
 }
 
-p{
-    color: red;
-    font-size: 20px;
-    margin-top: 0px;
+p {
+  color: white;
+  font-size: 20px;
+  margin-top: 0px;
 }
-h1, h2{
-    color: red;
-
+h1,
+h2 {
+  color: white;
 }
-h2{
-    margin-top: 150px;
+h2 {
+  margin-top: 150px;
+}
+.container {
+  position: relative;
+  z-index: 99; /*indice identificador*/
 }
 </style>
