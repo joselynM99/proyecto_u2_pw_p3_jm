c5568546d11a9bf54dd7af07400c17470123f62a
Joselyn Moncayo <joselynmoncayo1999@gmail.com>
2023-01-11 08:06:28 -0500

    tarea 7 Calculadora vue

---
+++ src/components/AppP.vue
@@ -0,0 +1,24 @@
+<template>
+  <Pregunta></Pregunta>
+</template>
+
+<script>
+import Pregunta from "./Pregunta.vue";
+export default {
+  name: "App",
+  components: {
+    Pregunta,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Tecla.vue
@@ -0,0 +1,60 @@
+<template>
+  <button class="teclas" v-on:click="accion(valor, calc, reset)">
+    {{ valor }}
+  </button>
+</template>
+
+<script>
+export default {
+  name: "Tecla",
+  props: {
+    valor: String,
+    calc: Boolean,
+    reset: Boolean,
+  },
+
+  data() {
+    return {
+      valor: this.valor,
+      calc: this.calc,
+      reset: this.reset,
+    };
+  },
+
+  methods: {
+    presionar(n) {
+      var resultado = document.getElementById("resultado").value;
+      if (resultado != "0" || isNaN(n))
+        document.getElementById("resultado").value = resultado + n;
+      else document.getElementById("resultado").value = n;
+    },
+    calcular() {
+      var resultado = eval(document.getElementById("resultado").value);
+      document.getElementById("resultado").value = resultado;
+    },
+    resetear() {
+      document.getElementById("resultado").value = 0;
+    },
+
+    accion(val, c, res) {
+      if (c == true) {
+        this.calcular();
+        console.log("cal");
+      } else if (res == true) {
+        this.resetear();
+        console.log("res");
+      } else {
+        this.presionar(val);
+        console.log("pres");
+      }
+    },
+  },
+};
+</script>
+
+<style>
+.teclas {
+  width: 70px;
+  height: 70px;
+}
+</style>
